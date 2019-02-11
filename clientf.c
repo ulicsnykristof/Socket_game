5b0e208ba6569bac274b4b9f949276c753eb6d4d
ulicsnykristof <44730988+ulicsnykristof@users.noreply.github.com>
2019-02-11 06:56:41 +0100

    Add files via upload

---
+++ clientf.c
@@ -0,0 +1,285 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/file.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <math.h>
+#include <ctype.h>
+
+#define SERVER_ADDR "127.0.0.1"
+#define PORT_NO 6066
+
+
+void hiba(char *tipus){	
+	if(strcmp(tipus, "send")==0){
+		perror("Send hiba\n");
+		exit(1);
+	}else if(strcmp(tipus, "recv")==0){
+		perror("Receive hiba\n");
+		exit(1);
+	}else if(strcmp(tipus, "accept")==0){
+		perror("Accept hiba\n");
+		exit(1);
+	}else if(strcmp(tipus, "socket")==0){
+		perror("Socket hiba\n");
+		exit(1);
+	}else if(strcmp(tipus, "connect")==0){
+		perror("Connect hiba\n");
+		exit(1);
+	}else{
+		perror("Listen hiba\n");
+		exit(1);
+	}
+}
+
+int ending(){
+	printf("Szeretnél új játékot kezdeni?\n (ujra / vege)\n");
+	while(1){
+		char *t;
+		scanf("%s", t);
+		if(strcmp(t, "ujra") == 0){
+			return 1;
+		}else if(strcmp(t, "vege") == 0){
+			return 0;
+		}else{
+			fprintf(stderr, "Rossz parancs\n (ujra / vege)\n");
+		}
+	}
+}
+
+
+int main(){
+/*************************************************************************/	
+	int fd, fdc;
+	int flags = 0;
+	struct sockaddr_in server;
+	struct sockaddr_in client;
+	int server_size = sizeof server;
+	int client_size = sizeof client;	
+	char sendBuff[1025];
+	char receBuff[1025];
+	int bytes = 1024;
+	int ip;
+	int on = 1;
+	char *ipstr;
+	int my_coin = 0;
+	int tovabb;
+	int kezdo_coin;
+/*************************************************************************/
+	ip = inet_addr(SERVER_ADDR);
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = ip;
+	server.sin_port = htons(PORT_NO);
+/*************************************************************************/
+	if((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) hiba("socket");
+
+	setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof on);
+	setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, (char *)&on, sizeof on);
+	
+	if(connect(fd, (struct sockaddr *) &server, server_size) == -1) hiba("connect");
+	
+	//ipstr = inet_ntoa(server.sin_addr);
+	//printf("Connected to Server with IP address %s\n", ipstr);
+	
+	if(recv(fd, receBuff, bytes, flags) == -1) hiba("rece");
+	printf("%s\n", receBuff);
+	
+	if(recv(fd, receBuff, bytes, flags) == -1) hiba("rece");
+	my_coin = atoi(receBuff);
+	kezdo_coin = my_coin;
+	
+	printf("Kezdő zsetonok száma = %d\n", my_coin);
+/*************************************************************************/	
+/*******KEZDÉS******************************************************************/		
+	while(1){
+		printf("Hány tétet szeretnél rakni? (max 3)\n");
+		printf("Ha be szeretnéd fejezni a játékot, írd be: befejezem\n");
+		
+		int befejezem=0;
+		int tetek_szama;
+		while(1){
+			int isdigi = 1;
+			char valasz[30];
+			scanf("%s", valasz);
+			int q;
+			for(q=0;q<strlen(valasz);q++){
+				if(!isdigit(valasz[q])) isdigi=0;
+			}
+			
+			if(isdigi==1){
+				tetek_szama=atoi(valasz);
+				if(tetek_szama>3 || tetek_szama<=0){
+					fprintf(stderr, "1, 2 vagy 3 zsetont tehetsz fel egyszerre!\n");
+				}else if(tetek_szama>my_coin){
+					fprintf(stderr, "Nincs elég zsetonod hozzá!\n");
+				}else{
+					break;
+				}
+			}else{
+				if(strcmp(valasz, "befejezem") == 0){
+					befejezem=1;
+					break;
+				}else{
+					fprintf(stderr, "Rossz parancs\n");
+				}
+			}
+		}
+		printf("Másik játékos választ...\n");
+		
+/************done**************************************************/
+		
+		sprintf(sendBuff, "%d", befejezem);
+		if(send(fd, sendBuff, bytes, flags) == -1) hiba("send");
+		
+		int ellen_befejez;
+		if(recv(fd, receBuff, bytes, flags) == -1) hiba("rece");
+		ellen_befejez = atoi(receBuff);
+		
+		if(befejezem==1 && ellen_befejez==1){
+			printf("Te és ellenfeled is befejzezte a játékot!\n");
+			break;
+		}else if(befejezem==1){
+			printf("Befejezted a játékot\n");
+			break;
+		}else if(ellen_befejez==1){
+			printf("Ellenfél befejezte a játékot\n");
+			break;
+		}
+		
+/******************************************************************/
+		
+		
+		printf("Hova szeretnéd tenni a zsetonokat? (0-9)\n");
+		int tetek_helye[3];
+		int k;
+		int hely;
+		char val[30];
+		for(k=0;k<tetek_szama;k++){
+			while(1){
+				printf("%d. zseton?\n", k+1);
+				scanf("%s", val);
+				int d;
+				int isdigi=1;
+				for(d=0;d<strlen(val);d++){
+					if(!isdigit(val[d])) isdigi=0;
+				}
+				if(isdigi==1){
+					hely=atoi(val);
+					if(hely>9 || hely<0){
+						fprintf(stderr, "0 és 9 közötti számot adj meg!\n");
+					}else if(k==1 && hely==tetek_helye[0] || k==2 && hely==tetek_helye[1]){
+						fprintf(stderr, "Itt már van zsetonod!\n");
+					}else{
+						tetek_helye[k] = hely;
+						break;
+					}
+				}else{
+					fprintf(stderr, "Számot adj meg légy oly kedves :)\n");
+				}
+			}
+		}
+		
+	
+		sprintf(sendBuff, "%d", tetek_szama);
+		if(send(fd, sendBuff, bytes, flags) == -1) hiba("send");
+		
+		
+		for(k=0;k<tetek_szama;k++){
+			sprintf(sendBuff, "%d", tetek_helye[k]);
+			if(send(fd, sendBuff, bytes, flags) == -1) hiba("send");
+		}
+		
+		printf("Másik játékos tétet tesz...\n");
+		
+/*****EGYES********************************************************************/	
+		
+/******KETTES*******************************************************************/			
+		
+		if(recv(fd, receBuff, bytes, flags) == -1) hiba("rece");
+		
+		printf("\n**********************************\n");
+		printf("Játék állása:\n");
+		printf("-------------\n\n");
+		
+		if(my_coin > atoi(receBuff)){
+			printf("Nincs találat\n");
+		}else{
+			printf("Találat\n");
+		}		
+		my_coin = atoi(receBuff);
+		
+		if(recv(fd, receBuff, bytes, flags) == -1) hiba("rece");
+		printf("Pörgetett szám: %d\n", atoi(receBuff));
+		printf("Zsetonok száma: %d\n", my_coin);
+		
+		if(recv(fd, receBuff, bytes, flags) == -1) hiba("rece");
+		int enemy_coin = atoi(receBuff);
+		
+		printf("Ellenfél zsetonjai: %d\n", enemy_coin);
+		
+		printf("\n**********************************\n\n");	
+		
+/******HÁRMAS*******************************************************************/		
+		
+		int ujra, ellen_ujra;
+		
+		if(recv(fd, receBuff, bytes, flags) == -1) hiba("rece");
+		int vege = atoi(receBuff);
+		if(recv(fd, receBuff, bytes, flags) == -1) hiba("rece");
+		int ellen_halt = atoi(receBuff);
+		if(vege==1){
+			if(ellen_halt==1 && my_coin > 0){
+				printf("GYŐZTÉL!!\n");
+			}else if(ellen_halt==1 && my_coin <= 0){
+				printf("DÖNTETLEN!!\n");
+			}else{
+				printf("VESZTETTÉL!!\n");
+			}
+			printf("A játék véget ért\n");
+			ujra = ending();
+		}else if(vege==2){
+			printf("Elfogytak a körök!\n");
+			if(my_coin > enemy_coin){
+				printf("Több zsetonod maradt, szóval győztél(?)\n");
+			}else if(my_coin < enemy_coin){
+				printf("Kevesebb zsetonod maradt, szóval vesztettél(?)\n");
+			}else{
+				printf("Ugyanannyi zsetonod maradt, szóval döntetlen(?)\n");
+			}
+			ujra = ending();
+		}
+		
+/******NÉGYES*******************************************************************/
+		
+		if(vege == 1 || vege == 2){
+			sprintf(sendBuff, "%d", ujra);
+			if(send(fd, sendBuff, bytes, flags) == -1) hiba("send");
+		
+			if(recv(fd, receBuff, bytes, flags) == -1) hiba("rece");
+			ellen_ujra = atoi(receBuff);
+		
+			if(ujra == 0){
+				printf("Nem szeretnél új játékot kezdeni\n");
+				printf("Köszönjük hogy minket választott\n");
+				break;
+			}else if(ellen_ujra == 0){
+				printf("Ellenfeled nem szeretne új játékot kezdeni\n");
+				printf("Köszönjük hogy minket választott\n");
+				break;
+			}else{
+				printf("Új játék indítása\n");
+				my_coin = kezdo_coin;
+				printf("Kezdő zsetonok száma = %d\n", my_coin);
+			}
+		}
+
+	}
+	close(fd);
+	close(fdc);
+	return 0;
+}
---
+++ serverf.c
@@ -0,0 +1,328 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/file.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <unistd.h>
+
+#define PORT_NO 6066
+
+
+void hiba(char *tipus){	
+	if(strcmp(tipus, "send")==0){
+		perror("Send hiba\n");
+		exit(1);
+	}else if(strcmp(tipus, "recv")==0){
+		perror("Receive hiba\n");
+		exit(1);
+	}else if(strcmp(tipus, "accept")==0){
+		perror("Accept hiba\n");
+		exit(1);
+	}else if(strcmp(tipus, "socket")==0){
+		perror("Socket hiba\n");
+		exit(1);
+	}else if(strcmp(tipus, "bind")==0){
+		perror("Bind hiba\n");
+		exit(1);
+	}else{
+		perror("Listen hiba\n");
+		exit(1);
+	}
+}
+
+void kiir(int i, int tomb_hossz, int tomb[]){
+	int k;
+	printf("*************************\n");
+	printf("%d. játékos tétje:\n", i);
+	printf("------------------\n");
+	for(k=0;k<tomb_hossz;k++){
+		printf("%d. tétje a %d számon\n", k+1, tomb[k]);
+	}
+	printf("*************************\n\n");
+
+}
+
+int main(int argc, char *argv[]){
+/***************************************************************************/
+/***Változók****************************************************************/	
+	int i;
+	int kezdo_coin, korok_szama;
+	int coin[2];
+	int fd, fdc[2];
+	int ip;
+	char *ipstr;
+	struct sockaddr_in server;
+	struct sockaddr_in client;
+	int server_size = sizeof server;
+	int client_size = sizeof client;
+	char sendBuff[1025];
+	char receBuff[1025];
+	int bytes = 1024;
+	int flags = 0;
+	int on = 1;
+	int halt[2];
+	int vege = 0;
+/***************************************************************************/
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(PORT_NO);
+	
+	kezdo_coin = atoi(argv[2]);
+	korok_szama = atoi(argv[1]);
+	coin[0] = kezdo_coin;
+	coin[1] = kezdo_coin;
+	halt[0] = 0;
+	halt[1] = 0;
+
+	srand(time(NULL));
+/***************************************************************************/
+	
+	if((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) hiba("socket");
+
+	setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof on);
+	setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, (char *)&on, sizeof on);
+	
+	if(bind(fd, (struct sockaddr *) &server, server_size) == -1) hiba("bind");
+	
+	printf("Rulett játék\n");
+	printf("Játékosok csatlakozására vár...\n\n");
+	
+/***************************************************************************/
+	i = 0;
+	while(i < 2){
+		if(listen(fd, 2) == -1) hiba("listen");	
+		if((fdc[i] = accept(fd, (struct sockaddr *) &client, &client_size)) == -1) hiba("accept");
+
+		ip = client.sin_addr.s_addr;
+    	ipstr = inet_ntoa(server.sin_addr);
+	
+		sprintf(sendBuff, "Köszöntelek a játékban!\n");
+		if(send(fdc[i], sendBuff, bytes, flags) == -1) hiba("send");
+		
+		printf("%d. Játékos becsatlakozott.\n", i);
+		if(i==0) printf("Várakozás a 2. játékosra...\n");
+		i++;
+	}
+		
+	i = 0;
+	while(i<2){
+		sprintf(sendBuff, "%d",	kezdo_coin);
+		if(send(fdc[i], sendBuff, bytes, flags) == -1) hiba("send");	
+		i++;
+	}
+	printf("\n");
+/***************************************************************************/
+/***************KEZDÉS******************************************************/	
+	int n = 0;
+	while(1){
+		n++;
+		printf("%d. kör, a játékosok tegyék meg tétjeiket\n", n);
+		
+/*done**********************************************************************/		
+		i=0;
+		int befejez[2];
+		while(i<2){
+			if(recv(fdc[i], receBuff, bytes, flags) == -1) hiba("recv");
+			befejez[i] = atoi(receBuff);
+			i++;
+		}
+		
+		sprintf(sendBuff, "%d", befejez[1]);
+		if(send(fdc[0], sendBuff, bytes, flags) == -1) hiba("send");			
+		sprintf(sendBuff, "%d",	befejez[0]);
+		if(send(fdc[1], sendBuff, bytes, flags) == -1) hiba("send");
+		
+		if(befejez[0]==1 && befejez[1]==1){
+			printf("Mindkét játékos befejzezte a játékot!\n");
+			break;
+		}else if(befejez[0]==1){
+			printf("Az első játékos befejezte a játékot\n");
+			break;
+		}else if(befejez[1]==1){
+			printf("A második játékos befejezte a játékot\n");
+			break;
+		}
+		
+/*done***********************************************************************/		
+				
+		int tetek_szama[2];
+		i=0;
+		while(i<2){
+			if(recv(fdc[i], receBuff, bytes, flags) == -1) hiba("recv");
+			tetek_szama[i] = atoi(receBuff);
+			i++;
+		}
+		
+		int tetek_helye_elso[3];
+		int tetek_helye_masodik[3];
+		
+		i=0;
+		while(i<2){
+			int k;
+			for(k=0;k<tetek_szama[i];k++){
+				if(recv(fdc[i], receBuff, bytes, flags) == -1) hiba("recv");
+				if(i==0) tetek_helye_elso[k] = atoi(receBuff);
+				else tetek_helye_masodik[k] = atoi(receBuff);
+			}
+			i++;
+		}
+
+		kiir(1, tetek_szama[0], tetek_helye_elso);
+		kiir(2, tetek_szama[1], tetek_helye_masodik);
+		
+/*****EGYES********************************************************************/
+		
+		int g=0;
+		printf("Pörög a kerék");
+		while(g<4){
+			printf(".");
+			sleep(1);
+			g++;
+		}
+		printf("\n\n");
+		
+		int porgetett = rand()%10;
+		coin[0] = coin[0] - tetek_szama[0];
+		coin[1] = coin[1] - tetek_szama[1];
+		
+		int z;
+		for(z=0;z<tetek_szama[0];z++){
+			if(porgetett == tetek_helye_elso[z]){
+				if(tetek_szama[0] == 1){
+					coin[0] = coin[0]+6*tetek_szama[0];
+				}else if(tetek_szama[0] == 2){
+					coin[0] = coin[0]+3*tetek_szama[0];
+				}else{
+					coin[0] = coin[0]+2*tetek_szama[0];
+				}
+			}
+		}
+		
+		for(z=0;z<tetek_szama[1];z++){
+			if(porgetett == tetek_helye_masodik[z]){
+				if(tetek_szama[1] == 1){
+					coin[1] = coin[1]+6*tetek_szama[1];
+				}else if(tetek_szama[1] == 2){
+					coin[1] = coin[1]+3*tetek_szama[1];
+				}else{
+					coin[1] = coin[1]+2*tetek_szama[1];
+				}
+			}
+		}
+		
+		printf("***************************\n\n");
+		printf("A pörgetett szám a(z): %d\n\n", porgetett);
+		printf("Játék állása:\n");
+		printf("-------------\n\n");
+		printf("Első játékos zsetonjai: %d\n", coin[0]);
+		printf("Második játékos zsetonjai: %d\n", coin[1]);
+		printf("\n***************************\n\n");
+/*******KETTES******************************************************************/				
+		
+		i=0;
+		while(i<2){
+			sprintf(sendBuff, "%d",	coin[i]);
+			if(send(fdc[i], sendBuff, bytes, flags) == -1) hiba("send");
+			i++;
+		}
+		
+		i=0;
+		while(i<2){
+			sprintf(sendBuff, "%d", porgetett);
+			if(send(fdc[i], sendBuff, bytes, flags) == -1) hiba("send");
+			i++;
+		}			
+					
+		sprintf(sendBuff, "%d",	coin[1]);
+		if(send(fdc[0], sendBuff, bytes, flags) == -1) hiba("send");			
+		sprintf(sendBuff, "%d",	coin[0]);
+		if(send(fdc[1], sendBuff, bytes, flags) == -1) hiba("send");			
+					
+/******HÁRMAS*******************************************************************/						
+										
+		if(coin[0] <= 0) halt[0] = 1;	
+		if(coin[1] <= 0) halt[1] = 1;
+		if(halt[0]==1 || halt[1]==1) vege = 1;
+		if(n == korok_szama) vege = 2;
+		
+		i=0;
+		while(i<2){
+			sprintf(sendBuff, "%d",	vege);
+			if(send(fdc[i], sendBuff, bytes, flags) == -1) hiba("send");
+			i++;
+		}			
+					
+		sprintf(sendBuff, "%d",	halt[1]);
+		if(send(fdc[0], sendBuff, bytes, flags) == -1) hiba("send");
+		
+		sprintf(sendBuff, "%d",	halt[0]);
+		if(send(fdc[1], sendBuff, bytes, flags) == -1) hiba("send");			
+		
+		if(vege==1){
+			printf("*******************************\n");
+			printf("Játszma vége\n\n");
+			printf("1. játékos zsetonjai = %d\n", coin[0]);
+			printf("2. játékos zsetonjai = %d\n", coin[1]);
+			if(coin[0]>coin[1]){
+				printf("1. játékos nyert\n");
+			}else if(coin[1]>coin[0]){
+				printf("2. játékos nyert\n");
+			}else{
+				printf("Döntetlen\n");
+			}
+			printf("*******************************\n");
+		}else if(vege==2){
+			printf("*******************************\n");
+			printf("Játszma vége, elfogytak a körök\n\n");
+			printf("1. játékos zsetonjai = %d\n", coin[0]);
+			printf("2. játékos zsetonjai = %d\n\n", coin[1]);
+			if(coin[0]>coin[1]){
+				printf("1. játékos nyert\n");
+			}else if(coin[1]>coin[0]){
+				printf("2. játékos nyert\n");
+			}else{
+				printf("Döntetlen\n");
+			}
+			printf("*******************************\n");
+		}
+		
+/******NÉGYES*******************************************************************/
+		
+		int ujra[2];
+		if(vege == 1 || vege == 2){
+			i=0;
+			while(i<2){
+				if(recv(fdc[i], receBuff, bytes, flags) == -1) hiba("recv");
+				ujra[i] = atoi(receBuff);
+				i++;
+			}
+		
+			sprintf(sendBuff, "%d",	ujra[1]);
+			if(send(fdc[0], sendBuff, bytes, flags) == -1) hiba("send");
+			sprintf(sendBuff, "%d",	ujra[0]);
+			if(send(fdc[1], sendBuff, bytes, flags) == -1) hiba("send");
+			
+			if(ujra[0] == 0 || ujra[1] == 0){
+				printf("Játékosok befejezték a játékot, jöjjenek máskor is!\n");
+				break;
+			}else{
+				printf("Új játék indul\n");
+				coin[0] = kezdo_coin;
+				coin[1] = kezdo_coin;
+				halt[0] = 0;
+				halt[1] = 0;
+				vege = 0;
+				n = 0;
+			}
+		}
+		
+		
+	}
+	close(fd);
+	close(fdc[0]);
+	close(fdc[1]);
+	return 0;
+}
